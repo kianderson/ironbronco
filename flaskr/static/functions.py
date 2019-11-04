e7943bb57e4eaead53d6411efdf61d3ca74e8653
Kristina Anderson <kianderson@Kristinas-MacBook-Pro.local>
2019-11-04 08:53:21 -0800

    participant and team classes added

--- flaskr/static/functions.py
+++ flaskr/static/functions.py
@@ -2,19 +2,48 @@
 import requests
 import json
 
-# Pool class has attributes name, pool id, and inventory (a list of dictionaries: dict = {"machine": ws-####-##, "status": status})
-class Pool:
+# Participant class
+class Participant:
     name = None
-    pool_id = None
-    inventory = {}
+    email = None
+    major = None
+    team = None
+    need = None
 
-    def __init__(self, name, pool_id, inventory):
-        self.name = name[7:] # from after "shared-" to end of string
-        self.pool_id = pool_id
-        self.inventory = inventory
+    def __init__(self, name, email, major, team, need):
+        self.name = name
+        self.email = email
+        self.major = major
+        self.team = team
+        self.need = need
 
     def __repr__(self):
         return self.name
+
+# Participant class
+class Team:
+    participant1 = None
+    participant2 = None
+    participant3 = None
+    progress = {}
+
+    def __init__(self, participant1, participant2, participant3):
+        self.participant1 = participant1
+        self.participant2 = participant2
+        self.participant3 = participant3
+        progress = {"Bike": 0, "Run": 0, "Swim": 0}
+
+    def __repr__(self):
+        return self.name
+
+# progress functions
+def logProgress(b, r, s):
+    return
+
+# TODO:
+# progress board html
+# pricing on profile Board
+# self progress
 
 # ------------------------------------------------------------------------------
 # GET
